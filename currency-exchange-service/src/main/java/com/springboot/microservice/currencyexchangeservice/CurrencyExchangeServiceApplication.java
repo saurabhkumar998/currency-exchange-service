438775c73c656a9f97706f8e82eae0aba75ecff6
saurabhkumar998 <saurabh.kumar.2382000@gmail.com>
2023-01-26 23:54:21 +0530

    initialized JpaRepository

--- currency-exchange-service/src/main/java/com/springboot/microservice/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/springboot/microservice/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -1,7 +1,9 @@
 package com.springboot.microservice.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values(10002,'USD','INR',65,'');
+
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values(10003,'EUR','INR',75,'');
+
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
+values(10004,'CAD','INR',45,'');
