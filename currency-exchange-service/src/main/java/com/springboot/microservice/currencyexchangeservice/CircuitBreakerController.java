bd532a22026c26f78f38e400d5b8a3cfee9e45a3
saurabhkumar998 <saurabh.kumar.2382000@gmail.com>
2023-01-29 23:54:56 +0530

    implemented RateLimiter and Bulkhead

--- currency-exchange-service/src/main/java/com/springboot/microservice/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/springboot/microservice/currencyexchangeservice/CircuitBreakerController.java
@@ -2,10 +2,8 @@ package com.springboot.microservice.currencyexchangeservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import io.github.resilience4j.retry.annotation.Retry;
 
@@ -16,10 +14,14 @@ public class CircuitBreakerController {
 	
 	@GetMapping("/sample-api")
 	@Retry(name="sample-api", fallbackMethod="hardCodedResponse")			// name is used to for the configuration in the properties file
+// 	@CircuitBreaker(name="sample-api", fallbackMethod="hardCodedResponse")
+//	@RateLimiter(name="sample-api")
+//	@Bulkhead(name="sample-api")
 	public String sampleApi() {
 		logger.info("Sample API call received");
-		ResponseEntity<String> forEntity =  new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-		return forEntity.getBody();
+//		ResponseEntity<String> forEntity =  new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+//		return forEntity.getBody();
+		return "Sample API!!!!";
 	}
 	
 	public String hardCodedResponse(Exception ex) {
